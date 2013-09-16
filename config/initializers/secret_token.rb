269406e1f9c8544cdbb864e18706fa43483b7143
Jun Lin <linjunpop@gmail.com>
2013-09-16 17:12:06 +0800

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Packor::Application.config.secret_key_base = '29bda5eeb11995e14e85d61a9014123d72b0e4795d978094496de5ffbd2b56e947ba067b2d2e14c48b739ccfb15f790a1f177b0e3d553e72f11e27b8b27d44c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Packor::Application.config.session_store :cookie_store, key: '_Packor_session'
